df922c05cd7a0e33c6cc7f9beb920ed5099f768e
Martin Keegan <martin@no.ucant.org>
2013-06-22 18:25:59 +0100

    fix year

--- src/server/commands.mli
+++ src/server/commands.mli
@@ -1,7 +1,7 @@
 (*
   Archipelago, a multi-user dungeon (MUD) server, by Martin Keegan
 
-  Copyright (C) 2009-2012  Martin Keegan
+  Copyright (C) 2009-2013  Martin Keegan
 
   This programme is free software; you may redistribute and/or modify
   it under the terms of the GNU Affero General Public Licence as published by
