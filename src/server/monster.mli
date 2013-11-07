9fdfa7434ea9e73f633a00aa1c82046cf152016a
Martin Keegan <martin@no.ucant.org>
2013-11-07 21:40:46 +0000

    add copyright boilerplate

--- src/server/monster.mli
+++ src/server/monster.mli
@@ -1,3 +1,13 @@
+(*
+  Archipelago, a multi-user dungeon (MUD) server, by Martin Keegan
+
+  Copyright (C) 2009-2013  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the GNU Affero General Public Licence as published by
+  the Free Software Foundation, either version 3 of said Licence, or
+  (at your option) any later version.
+*)
 
 val add_daemon : Model.mudobject -> unit
 val pump_events : unit -> unit
