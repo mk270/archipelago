7e866487d747bc75a0194368111354bc31804fc5
Martin Keegan <martin@no.ucant.org>
2014-03-20 18:37:48 +0000

    put interface on pack

---
+++ src/utils/utils.mli
@@ -0,0 +1,8 @@
+
+module Ext : sig
+	include Utils_sigs.Ext
+end
+
+module Fsm : sig
+	include Utils_sigs.Fsm
+end
--- src/utils/utils_sigs.ml
+++ src/utils/utils_sigs.ml
@@ -1,3 +1,4 @@
+module type Fsm = sig
 type ('response, 'message, 'state) fsm = {
 	mutable fsm_state : 'state;
 	fsm_input_handler : ('state * 'message) -> ('state * 'response)
@@ -7,3 +8,20 @@ val create : 'state -> (('state * 'message) -> ('state * 'response)) ->
 	('response, 'message, 'state) fsm
 
 val send : ('response, 'message, 'state) fsm -> 'message -> 'response
+end
+
+module type Ext = sig
+
+val remove : 'a -> 'a list -> 'a list
+
+val comma_sep : string list -> string
+
+val initial_match : string -> string -> bool
+
+val random_list_member : 'a list -> 'a
+
+val guard_exception : exn -> unit
+
+val tally : 'a list -> ('a * int) list
+
+end
